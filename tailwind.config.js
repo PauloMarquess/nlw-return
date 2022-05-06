51d88e5ff7a14bfca45f468d0dfd325acaa5d60d
Paulo Marques <paulomarques@rethinks-MacBook-Pro.local>
2022-05-06 09:50:50 -0300

    feat: botao submit e botao de captura de tela

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,14 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        brand:{500:"#8b5cf6"}
+        brand:{500:"#8b5cf6"},
+        // brand:{300:"#996DFF"},
       }
     },
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('tailwind-scrollbar')
     
   ],
 }
