e8214a887a5a2bec6af86af11248278b72d93d45
Paulo Marques <paulomarques@rethinks-MacBook-Pro.local>
2022-05-06 09:26:12 -0300

    bug: bug na scroll bar

--- src/components/Steps/FeedbackContentSteps.tsx
+++ src/components/Steps/FeedbackContentSteps.tsx
@@ -1,3 +1,49 @@
-export const FeedBackContentSteps = () => {
-  return <div>ss</div>;
+import { ArrowLeft } from 'phosphor-react';
+import { FeedbackTypes, feedbackTypes } from '../../__mocks__';
+import { CloseButton } from '../CloseButton';
+
+interface FeedBackContentSteps {
+  feedbackType: FeedbackTypes;
+  onFeedbackRestartRequestd: () => void;
+}
+
+export const FeedBackContentSteps = ({
+  feedbackType,
+  onFeedbackRestartRequestd,
+}: FeedBackContentSteps) => {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+  return (
+    <>
+      <header>
+        <button
+          type="button"
+          onClick={onFeedbackRestartRequestd}
+          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+        </button>
+        <span className="text-xl leading-6 flex items-center gap-2 justify-center ">
+          <img
+            className="w-6 h-6"
+            src={feedbackTypeInfo.image.source}
+            alt={feedbackTypeInfo.image.alt}
+          />
+          {feedbackTypeInfo.title}
+        </span>
+        <CloseButton />
+      </header>
+      <form className="my-4 w-full">
+        <textarea
+          className="min-w-[304px] w-full min-h-[104px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 focus:outline-none focus:border-brand-500 bg-transparent rounded-md focus:ring-brand-500 focus:ring-1 resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thi"
+          placeholder="Conte com detalhes o que está acontecendo..."
+        ></textarea>
+        <footer>
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover-bg-brand-300"
+          ></button>
+        </footer>
+      </form>
+    </>
+  );
 };
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,20 +1,23 @@
 import { useState } from 'react';
 import { feedbackTypes } from '../../__mocks__';
 import { CloseButton } from '../CloseButton';
+import { FeedBackContentSteps } from '../Steps/FeedbackContentSteps';
 import { FeedBackTypesSteps } from '../Steps/FeedbackTypesSteps';
 
 export const WidgetForm = () => {
   const [feedbackType, setFeedbackType] = useState<any | null>(null);
+  const handleRestartFeedback = () => {
+    setFeedbackType(null);
+  };
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu Feedback</span>
-        <CloseButton />
-      </header>
       {!feedbackType ? (
         <FeedBackTypesSteps onFeedbackTypeStep={setFeedbackType} />
       ) : (
-        <p>Hello World?</p>
+        <FeedBackContentSteps
+          onFeedbackRestartRequestd={handleRestartFeedback}
+          feedbackType={feedbackType}
+        />
       )}
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela &nbsp;
